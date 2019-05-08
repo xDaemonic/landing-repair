118d92c32750a3b89a7577a72c4f07b90d04afc9
Vyacheslav <errewer123@gmail.com>
2019-05-08 16:13:26 +0300

    finished 5 sections

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$('.work-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<div class="left-arrow"></div>',
+  nextArrow: '<div class="right-arrow"></div>',
+});
